313f1ca25e8d689fdebd3c0386a2da2bd52fcdec
BurnCan <burncan@proton.me>
2025-06-08 09:31:28 -0400

    commented out processInput(window); in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -109,7 +109,7 @@ int main()
 
     while (!glfwWindowShouldClose(window))
     {
-        processInput(window);
+        //processInput(window);
 
         glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
