099591fa918eb15cbd23501ec3eb33d0fbfaabdd
BurnCan <burncan@proton.me>
2025-06-08 14:23:15 -0400

    changed GLFW_CURSOR_DISABLED to GLFW_CURSOR_NORMAL

--- src/main.cpp
+++ src/main.cpp
@@ -105,7 +105,8 @@ int main()
     
     glfwSetCursorPosCallback(window, mouse_callback);
     glfwSetScrollCallback(window, scroll_callback);
-    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+
 
     while (!glfwWindowShouldClose(window))
     {
