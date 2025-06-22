5c97d8ae11d90925e8ce3e62671ea19f09bc416d
BurnCan <burncan@proton.me>
2025-06-22 11:25:10 -0400

    stripped color data from cube vertices and changed stride of 6 floats to a stride of 3 floats in glVertexAttribPointer since we no longer have floats for color data

--- src/main.cpp
+++ src/main.cpp
@@ -92,59 +92,59 @@ int main()
 
     //Cube
     float vertices[] = {
-        // Back face (z = -0.5)
-        -0.5f, -0.5f, -0.5f,   1.0f, 0.0f, 0.0f,  // Bottom-left
-         0.5f,  0.5f, -0.5f,   0.0f, 1.0f, 0.0f,  // Top-right
-         0.5f, -0.5f, -0.5f,   0.0f, 0.0f, 1.0f,  // Bottom-right
+        // Back face
+        -0.5f, -0.5f, -0.5f,
+         0.5f,  0.5f, -0.5f,
+         0.5f, -0.5f, -0.5f,
 
-        -0.5f, -0.5f, -0.5f,   1.0f, 0.0f, 0.0f,  // Bottom-left
-        -0.5f,  0.5f, -0.5f,   1.0f, 1.0f, 0.0f,  // Top-left
-         0.5f,  0.5f, -0.5f,   0.0f, 1.0f, 0.0f,  // Top-right
+        -0.5f, -0.5f, -0.5f,
+        -0.5f,  0.5f, -0.5f,
+         0.5f,  0.5f, -0.5f,
 
-         // Front face (z = +0.5)
-         -0.5f, -0.5f,  0.5f,   0.0f, 1.0f, 1.0f,
-          0.5f, -0.5f,  0.5f,   1.0f, 0.0f, 1.0f,
-          0.5f,  0.5f,  0.5f,   0.5f, 0.5f, 0.5f,
+         // Front face
+         -0.5f, -0.5f,  0.5f,
+          0.5f, -0.5f,  0.5f,
+          0.5f,  0.5f,  0.5f,
 
-         -0.5f, -0.5f,  0.5f,   0.0f, 1.0f, 1.0f,
-          0.5f,  0.5f,  0.5f,   0.5f, 0.5f, 0.5f,
-         -0.5f,  0.5f,  0.5f,   0.2f, 0.2f, 0.8f,
+         -0.5f, -0.5f,  0.5f,
+          0.5f,  0.5f,  0.5f,
+         -0.5f,  0.5f,  0.5f,
 
-         // Left face (x = -0.5)
-         -0.5f, -0.5f, -0.5f,   0.8f, 0.3f, 0.3f,
-         -0.5f, -0.5f,  0.5f,   0.3f, 0.8f, 0.3f,
-         -0.5f,  0.5f,  0.5f,   0.3f, 0.3f, 0.8f,
+         // Left face
+         -0.5f, -0.5f, -0.5f,
+         -0.5f, -0.5f,  0.5f,
+         -0.5f,  0.5f,  0.5f,
 
-         -0.5f, -0.5f, -0.5f,   0.8f, 0.3f, 0.3f,
-         -0.5f,  0.5f,  0.5f,   0.3f, 0.3f, 0.8f,
-         -0.5f,  0.5f, -0.5f,   0.6f, 0.6f, 0.2f,
+         -0.5f, -0.5f, -0.5f,
+         -0.5f,  0.5f,  0.5f,
+         -0.5f,  0.5f, -0.5f,
 
-         // Right face (x = +0.5)
-          0.5f, -0.5f, -0.5f,   0.3f, 0.3f, 0.3f,
-          0.5f,  0.5f,  0.5f,   0.2f, 0.5f, 0.7f,
-          0.5f, -0.5f,  0.5f,   0.6f, 0.1f, 0.4f,
+         // Right face
+          0.5f, -0.5f, -0.5f,
+          0.5f,  0.5f,  0.5f,
+          0.5f, -0.5f,  0.5f,
 
-          0.5f, -0.5f, -0.5f,   0.3f, 0.3f, 0.3f,
-          0.5f,  0.5f, -0.5f,   0.9f, 0.9f, 0.1f,
-          0.5f,  0.5f,  0.5f,   0.2f, 0.5f, 0.7f,
+          0.5f, -0.5f, -0.5f,
+          0.5f,  0.5f, -0.5f,
+          0.5f,  0.5f,  0.5f,
 
-          // Top face (y = +0.5)
-          -0.5f,  0.5f, -0.5f,   1.0f, 0.0f, 0.0f,
-          -0.5f,  0.5f,  0.5f,   0.0f, 1.0f, 0.0f,
-           0.5f,  0.5f,  0.5f,   0.0f, 0.0f, 1.0f,
+          // Top face
+          -0.5f,  0.5f, -0.5f,
+          -0.5f,  0.5f,  0.5f,
+           0.5f,  0.5f,  0.5f,
 
-          -0.5f,  0.5f, -0.5f,   1.0f, 0.0f, 0.0f,
-           0.5f,  0.5f,  0.5f,   0.0f, 0.0f, 1.0f,
-           0.5f,  0.5f, -0.5f,   1.0f, 1.0f, 0.0f,
+          -0.5f,  0.5f, -0.5f,
+           0.5f,  0.5f,  0.5f,
+           0.5f,  0.5f, -0.5f,
 
-           // Bottom face (y = -0.5)
-           -0.5f, -0.5f, -0.5f,   0.3f, 0.3f, 0.3f,
-            0.5f, -0.5f,  0.5f,   0.5f, 0.2f, 0.7f,
-            0.5f, -0.5f, -0.5f,   0.7f, 0.2f, 0.5f,
+           // Bottom face
+           -0.5f, -0.5f, -0.5f,
+            0.5f, -0.5f,  0.5f,
+            0.5f, -0.5f, -0.5f,
 
-           -0.5f, -0.5f, -0.5f,   0.3f, 0.3f, 0.3f,
-           -0.5f, -0.5f,  0.5f,   0.1f, 0.8f, 0.5f,
-            0.5f, -0.5f,  0.5f,   0.5f, 0.2f, 0.7f,
+           -0.5f, -0.5f, -0.5f,
+           -0.5f, -0.5f,  0.5f,
+            0.5f, -0.5f,  0.5f,
     };
 
 
@@ -157,10 +157,10 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
+    //glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    //glEnableVertexAttribArray(1);
 
     //Initialize shaderProgram
     shaderProgram = createShaderProgramFromFile("basic.vert", "basic.frag");
